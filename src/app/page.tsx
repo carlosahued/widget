d1f867fabbb92c6dfd5482ef3c9174f07408dc8b
Carlos Pinto <carlospinto@Carloss-MacBook-Pro.local>
2024-07-02 21:06:50 -0400

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import Widget from "@/components/widget";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Bienvenido a mi sitio web</h1>
-      <Widget />
-    </div>
-  );
+  return <Widget />;
 }
