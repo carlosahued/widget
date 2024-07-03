c5c11958f28a6faa7ed29ae9beda8a335b59d56e
Carlos Pinto <carlospinto@Carloss-MacBook-Pro.local>
2024-07-02 23:18:31 -0400

    bu

---
+++ public/symbols/index.ts
@@ -0,0 +1,6 @@
+import Tv from "./Tv.svg";
+import App from "./App.svg";
+import VideoFill from "./VideoFill.svg";
+import AppDashed from "./AppDashed.svg";
+
+export { Tv, App, VideoFill, AppDashed };
--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -1,12 +1,17 @@
 // src/components/widget.tsx
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { App, Tv, VideoFill } from "../../public/symbols";
+import styles from "./styles.module.scss";
 
 const ChatWidget = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const [id, setId] = useState("");
+  const [open, setOpen] = useState(false);
+  const [start, setStart] = useState(false);
+  const audioRef: any = useRef(null);
+  const audioRefe: any = useRef(null);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -16,25 +21,82 @@ const ChatWidget = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (open && audioRef.current) {
+      audioRef.current.play();
+      setTimeout(() => {
+        audioRefe.current.play();
+      }, 1000);
+    }
+  }, [open]);
+
+  let imageUrl =
+    "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
   return (
     <div style={{ width: "435px", height: "400px", background: "red" }}>
-      <button onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? "Cerrar" : "Abrir"} Modal
-      </button>
-      <AnimatePresence>
-        {isOpen && (
+      <button onClick={() => setOpen(!open)}>{open ? "CLOSE" : "OPEN"}</button>
+      {/* MODAL */}
+      <AnimatePresence initial={false}>
+        {open && (
           <motion.div
+            className={styles.q}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            style={{
-              backgroundColor: "blue",
-            }}
           >
-            Hola Mundo ID: {id}
+            {/* HEADER */}
+            <div className={styles.w}>
+              <div className={styles.w2}>
+                <img src={imageUrl} alt="picture" />
+                <App />
+              </div>
+              <div className={styles.w3}>
+                <p>Clipme</p>
+                <p>Online</p>
+              </div>
+              <button className={styles.w4} onClick={() => setOpen(false)}>
+                <Tv />
+              </button>
+            </div>
+            {/* ACTIONS */}
+            <div className={styles.e1}>
+              <button>
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <span
+                    key={index}
+                    className={styles.bar}
+                    // style={{ height: `${barHeights[index]}px` }}
+                  ></span>
+                ))}
+              </button>
+              {start ? (
+                Array.from({ length: 3 }).map((_, i) => (
+                  <button key={i}>
+                    <Tv height="19" />
+                    <span className={styles.tool}>Screen</span>
+                  </button>
+                ))
+              ) : (
+                <button
+                  style={{ backgroundColor: "#32D74B" }}
+                  onClick={() => setStart(true)}
+                >
+                  <VideoFill />
+                  <p>Join</p>
+                </button>
+              )}
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
+      {/* AUDIO */}
+      <audio
+        ref={audioRef}
+        src="/sounds/o1.mp3"
+        preload="auto"
+        style={{ display: "none" }}
+      />
     </div>
   );
 };
